175afb4a60ea456a5dbafe7240d9f2d8265c863b
Hanne Mendoza <144074860+hanne-97@users.noreply.github.com>
2023-11-30 08:37:00 -0600

    Add files via upload

---
+++ S8_VendingGUI/src/VendingCall/VendingCall.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VendingCall;
+
+import Vending_GUI.VendingFront;
+//Grupo: Alejandra Reyes, Gerardo Hasbum, Hanne Mendoza, Maria Belén Padilla & Mia Elvir
+
+public class VendingCall {   
+
+    public static void main(String[] args) {
+        VendingFront panta = new VendingFront();
+        panta.setVisible(true);
+        //panta.setLocationRelativeTo(null);
+    }
+    
+}
+      
+
